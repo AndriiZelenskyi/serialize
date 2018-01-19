8888018746914514dbbc682468a77cae73eb1c26
Andrew Zelenskiy <andrii.zelenskyy@n-cube.co.uk>
2018-01-19 17:26:49 +0200

    Refactoring json utils

--- src/converters/json-utils.ts
+++ src/converters/json-utils.ts
@@ -3,28 +3,26 @@ export function parseJsonPropertyName(fullName: string): string[] {
 }
 
 export function setPropertyToJson(json: Object, propertyAddress: string[], value: any): Object {
-    const copyOfAddress = propertyAddress.map(v => '' + v);
-    const lastAddress = copyOfAddress.pop();
-    const tObject = copyOfAddress.reduce((pV: {[k: string]: any}, cV) => {
-        if (pV[cV] === undefined) {
-            pV[cV] = {};
-        }
-        return pV[cV];
-    }, json);
+    const {reduce, lastAddress} = reduceAddress(json, propertyAddress);
     if (lastAddress) {
-        tObject[lastAddress] = value;
+        reduce[lastAddress] = value;
     }
     return json;
 }
 
 export function getPropertyOfJson(json: Object, propertyAddress: string[]): Object | null {
+    const {reduce, lastAddress} = reduceAddress(json, propertyAddress);
+    return reduce[lastAddress || ''] || null;
+}
+
+function reduceAddress(json: Object, propertyAddress: string[]): {lastAddress: string, reduce: {[k: string]: any}} {
     const copyOfAddress = propertyAddress.map(v => '' + v);
-    const lastAddress = copyOfAddress.pop();
+    const lastAddress = copyOfAddress.pop() || '';
     const reduce: { [k: string]: any } = copyOfAddress.reduce((pV: { [k: string]: any }, cV) => {
         if (pV[cV] === undefined) {
             pV[cV] = {};
         }
         return pV[cV];
     }, json);
-    return reduce[lastAddress || ''] || null;
+    return { lastAddress, reduce}
 }
