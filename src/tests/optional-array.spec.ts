38dd40dfdc80d40e4cbde4f105d5d13bc66e64f8
Andrew Zelenskiy <andrii.zelenskyy@n-cube.co.uk>
2018-02-07 14:39:27 +0200

    Refactored to describe use cases

--- src/tests/optional-array.spec.ts
+++ src/tests/optional-array.spec.ts
@@ -1,7 +1,7 @@
 import {Field} from "../decorators";
 import {ArraySerializer, ModelSerializer} from "../serializers";
 import {PrimitiveSerializer} from "../serializers/primitive.serializer";
-import {serialize} from "../converters";
+import {deserialize, serialize} from "../converters";
 
 class TestConfiguration {
     @Field({serializer: new ArraySerializer(new PrimitiveSerializer())})
@@ -22,15 +22,23 @@ class TestModel {
 }
 
 describe('Optional model fields', () => {
-    const fullJSON = {
-        configuration: {
-            fields: ['a', 'b', 'c']
-        }
-    };
-    const fullModel = TestModel.getFull(['a', 'b', 'c']);
+    describe('Full model', () => {
 
-    it('should serialize full model with all fields', () => {
-        const serializedJSON = serialize(fullModel);
-        expect(serializedJSON).toEqual(fullJSON);
-    });
+        const fullJSON = {
+            configuration: {
+                fields: ['a', 'b', 'c']
+            }
+        };
+        const fullModel = TestModel.getFull(['a', 'b', 'c']);
+
+        it('should serialize', () => {
+            const serializedJSON = serialize(fullModel);
+            expect(serializedJSON).toEqual(fullJSON);
+        });
+
+        it('should deserialize', () => {
+            const deserializedModel = deserialize(fullJSON, TestModel);
+            expect(deserializedModel).toEqual(fullModel);
+        })
+    })
 });
