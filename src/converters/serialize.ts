15e8acdf3ae524ec942d33549fd9b3866daa5517
Andrew Zelenskiy <zvukach70@gmail.com>
2018-03-20 10:40:14 +0200

    Missing field serialization error (#7)
    * Add test for optional field
    
    * Refactored to describe use cases
    
    * Add test case for missing optional sub field
    
    * Add test case for missing sub model
    
    * Fix optional bug

--- src/converters/serialize.ts
+++ src/converters/serialize.ts
@@ -2,6 +2,8 @@ import {FIELDS_METADATA_KEY} from "../metadata.keys";
 import {FieldMetadata} from "../field/field.metadata";
 import {NoFieldsError} from "../errors";
 import {parseJsonPropertyName, setPropertyToJson} from "./json-utils";
+import 'reflect-metadata';
+import {isPresent} from "../serializers/field.utils";
 
 
 /**
@@ -12,19 +14,24 @@ import {parseJsonPropertyName, setPropertyToJson} from "./json-utils";
  * @param {Object} model Serializable model that was convert to json
  * @returns {Object} Server object
  */
-export function serialize(model: {[key: string]: any}): Object {
+export function serialize(model: { [key: string]: any }): Object {
     const modelPrototype = Object.getPrototypeOf(model);
     const fields = Reflect.getMetadata(FIELDS_METADATA_KEY, modelPrototype) as FieldMetadata[] | undefined;
 
-    if(fields === undefined) {
+    if (fields === undefined) {
         throw new NoFieldsError();
     }
 
     // Convert array of field metadata to json object
-    return fields.reduce((previousValue: {[k: string]: any}, currentValue: FieldMetadata) => {
+    return fields.reduce((previousValue: { [k: string]: any }, currentValue: FieldMetadata) => {
         const address = parseJsonPropertyName(<string>currentValue.jsonPropertyName);
-        const serializedModelValue = currentValue.serializer.serialize(model[currentValue.modelPropertyName]);
-        setPropertyToJson(previousValue, address, serializedModelValue);
+
+        const modelValue = model[currentValue.modelPropertyName];
+        if (isPresent(modelValue)) {
+            const serializedModelValue = currentValue.serializer.serialize(modelValue);
+            setPropertyToJson(previousValue, address, serializedModelValue);
+        }
+
         return previousValue;
     }, {});
 }
---
+++ src/tests/optional-array.spec.ts
@@ -0,0 +1,76 @@
+import {Field} from "../decorators";
+import {ArraySerializer, ModelSerializer} from "../serializers";
+import {PrimitiveSerializer} from "../serializers/primitive.serializer";
+import {deserialize, serialize} from "../converters";
+
+class TestConfiguration {
+    @Field({serializer: new ArraySerializer(new PrimitiveSerializer())})
+    fields?: Array<string>;
+}
+
+class TestModel {
+    @Field({serializer: new ModelSerializer(TestConfiguration)})
+    configuration?: TestConfiguration;
+
+    static getFull(fields: string[]): TestModel {
+        const model = new TestModel();
+        const configuration = new TestConfiguration();
+        configuration.fields = fields;
+        model.configuration = configuration;
+        return model;
+    }
+}
+
+describe('Optional model fields', () => {
+    describe('Full model', () => {
+
+        const fullJSON = {
+            configuration: {
+                fields: ['a', 'b', 'c']
+            }
+        };
+        const fullModel = TestModel.getFull(['a', 'b', 'c']);
+
+        it('should serialize', () => {
+            const serializedJSON = serialize(fullModel);
+            expect(serializedJSON).toEqual(fullJSON);
+        });
+
+        it('should deserialize', () => {
+            const deserializedModel = deserialize(fullJSON, TestModel);
+            expect(deserializedModel).toEqual(fullModel);
+        });
+
+    });
+
+    describe('Missing sub field', () => {
+        const JSON = {configuration: {}};
+        const model = new TestModel();
+        model.configuration = new TestConfiguration();
+
+        it('should serialize', () => {
+            const serializedJSON = serialize(model);
+            expect(serializedJSON).toEqual(JSON);
+        });
+
+        it('should deserialize', () => {
+            const deserializedModel = deserialize(JSON, TestModel);
+            expect(deserializedModel).toEqual(model);
+        });
+    });
+
+    describe('Missing sub model', () => {
+        const JSON = {};
+        const model = new TestModel();
+
+        it('should serialize', () => {
+            const serializedJSON = serialize(model);
+            expect(serializedJSON).toEqual(JSON);
+        });
+
+        it('should deserialize', () => {
+            const deserializedModel = deserialize(JSON, TestModel);
+            expect(deserializedModel).toEqual(model);
+        });
+    })
+});
