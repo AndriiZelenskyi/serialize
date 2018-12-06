9e87b6659a684f9fcf137a40a3d438ac1f58302f
Andrew Zelenskiy <drew.zelensk1y@gmail.com>
2018-12-06 11:00:34 +0200

    Add linter and add two decorators

--- src/field/field.metadata.ts
+++ src/field/field.metadata.ts
@@ -1,5 +1,6 @@
 import {FieldConfig} from "./field.config";
 import {Serializer} from "../serializers";
+import { Type } from "../type";
 
 export interface FieldMetadata extends FieldConfig {
     /**
@@ -11,4 +12,11 @@ export interface FieldMetadata extends FieldConfig {
      */
     jsonPropertyName: string;
     serializer: Serializer<any>;
+}
+
+export function getMetadata<T>(type: Type<T>): FieldMetadata[] {
+    const target = new type();
+    const properties: string[] = Object.getOwnPropertyNames(target);
+    // Reflect.getMetadata()
+    return [];
 }
--- src/type.ts
+++ src/type.ts
@@ -1,1 +1,8 @@
 export type Type<T extends Object> = {new(): T};
+
+export function isType<T>(candidate: any): candidate is Type<T> {
+    if(typeof candidate === 'function') {
+        return true;
+    }
+    return false;
+}
